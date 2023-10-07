80c23b2f56bbca4f4afb0dd06dc932569bd6e451
krononpark <57452349+krononpark@users.noreply.github.com>
2023-10-07 11:52:56 +0300

    battle city Все работает

---
+++ load.py
@@ -0,0 +1,11 @@
+import pygame
+
+player_image = pygame.image.load('image/player/player.png').convert_alpha()
+player_bullet = pygame.image.load('image/player/player bullet.png').convert_alpha()
+enemy_image = pygame.image.load('image/enemy/enemy.png').convert_alpha()
+enemy_bullet = pygame.image.load('image/enemy/enemy bullet.png').convert_alpha()
+brick_image = pygame.image.load('image/blocks/brick block.png').convert_alpha()
+iron_image = pygame.image.load('image/blocks/iron block.png').convert_alpha()
+bush_image = pygame.image.load('image/blocks/bush block.png').convert_alpha()
+water_image = pygame.image.load('image/blocks/water block.png').convert_alpha()
+flag_image = pygame.image.load('image/blocks/flag.png').convert_alpha()
---
+++ main.py
@@ -0,0 +1,153 @@
+import pygame
+import os
+import sys
+import random
+
+pygame.init()
+current_path = os.path.dirname(__file__)
+os.chdir(current_path)
+WIDTH = 1200
+HEIGHT = 800
+FPS = 60
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+pygame.display.set_caption('battle city')
+lvl = 'game'
+
+from load import *
+
+
+def lvlgame():
+    sc.fill("black")
+    brick_group.update()
+    brick_group.draw(sc)
+    bush_group.update()
+    bush_group.draw(sc)
+    iron_group.update()
+    iron_group.draw(sc)
+    water_group.update()
+    water_group.draw(sc)
+    enemy_group.update()
+    enemy_group.draw(sc)
+    player_group.update()
+    player_group.draw(sc)
+    flag_group.update()
+    flag_group.draw(sc)
+    pygame.display.update()
+
+
+def drawMaps(nameFile):
+    maps = []
+    source = "game lvl/" + str(nameFile)
+    with open(source, "r") as file:
+        for i in range(0, 20):
+            maps.append(file.readline().replace("\n", "").split(",")[0: -1])
+
+    pos = [0, 0]
+    for i in range(0, len(maps)):
+        pos[1] = i * 40
+        for j in range(0, len(maps[0])):
+            pos[0] = 40 * j
+            if maps[i][j] == '1':
+                brick = Brick(brick_image, pos)
+                brick_group.add(brick)
+            elif maps[i][j] == '2':
+                bush = Bush(bush_image, pos)
+                bush_group.add(bush)
+            elif maps[i][j] == '3':
+                iron = Iron(iron_image, pos)
+                iron_group.add(iron)
+            elif maps[i][j] == '4':
+                water = Water(water_image, pos)
+                water_group.add(water)
+            elif maps[i][j] == '6':
+                enemy = Enemy(enemy_image, pos)
+                enemy_group.add(enemy)
+            elif maps[i][j] == '5':
+                flag = Flag(flag_image, pos)
+                flag_group.add(flag)
+
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Bush(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Iron(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Water(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+class Flag(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+
+brick_group = pygame.sprite.Group()
+bush_group = pygame.sprite.Group()
+iron_group = pygame.sprite.Group()
+water_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+flag_group = pygame.sprite.Group()
+player = Player(player_image, (200, 640))
+player_group.add(player)
+
+drawMaps('1.txt')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    if lvl == 'game':
+        lvlgame()
+    clock.tick(FPS)
